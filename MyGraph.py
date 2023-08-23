f2c4c02589d6e47bc1b198677fa301063c99b654
Michael Ho <Michael_ Ho1@baylor.edu>
2023-08-23 15:37:40 -0500

    initial commit

---
+++ MyGraph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+## 1) create your Virtual environment
+## 2) Activate your VE
+## 3) Install 3rd party library in VE
+## make sure VE is activated and that you're using the VE interpretator
